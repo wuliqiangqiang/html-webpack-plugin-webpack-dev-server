41f7bf7e06857815b44e21c589e3bdbcd844a717
wuzhicheng <wuzhicheng@zuzuche.com>
2018-11-29 12:31:13 +0800

    ’JS注释兼容‘

--- index.js
+++ index.js
@@ -3,38 +3,40 @@ let hostName = os.hostname();
 let interfaces = os.networkInterfaces();
 let IPv4 = '127.0.0.1';
 for (var key in interfaces) {
-    var alias = 0;
-    interfaces[key].forEach((details) => {
-        if (details.family == 'IPv4' && key == 'en0') {
-            IPv4 = details.address;
-        }
-    });
+  var alias = 0;
+  interfaces[key].forEach((details) => {
+    if (details.family == 'IPv4' && key == 'en0') {
+      IPv4 = details.address;
+    }
+  });
 }
 
 function webpackAutoRefreshPlugin({ port, ip }) {
-    this.port = port;
-    this.ip = ip ? ip : IPv4;
+  this.port = port;
+  this.ip = ip ? ip : IPv4;
 }
 
 webpackAutoRefreshPlugin.prototype.apply = function(compiler) {
-    compiler.hooks.compilation.tap('webpackAutoRefreshPlugin', (compilation) => {
-        compilation.hooks.htmlWebpackPluginAfterHtmlProcessing.tapAsync(
-            'webpackAutoRefreshPlugin',
-            (data, cb) => {
-                if (!this.port) {
-                    cb(null, data);
-                } else {
-                    let html = data.html;
-                    html = html.replace(/\n/g, '');
-                    let fristScriptTapIndex = html.indexOf('</body');
-                    let reg = new RegExp('(.{' + fristScriptTapIndex + '})')
-                    html = html.replace(reg, `$1<script src="http://${this.ip?this.ip:'localhost'}:${this.port}/webpack-dev-server.js"></script>`);
-                    data.html = html;
-                    cb(null, data)
-                }
-            }
-        )
-    })
+  compiler.hooks.compilation.tap('webpackAutoRefreshPlugin', (compilation) => {
+    compilation.hooks.htmlWebpackPluginAfterHtmlProcessing.tapAsync(
+      'webpackAutoRefreshPlugin',
+      (data, cb) => {
+        if (!this.port) {
+          cb(null, data);
+        } else {
+          let html = data.html;
+          html = html.replace(/\/\/(.*)/g,'').replace(/\/\*(\n|.)*?\*\//g,'');
+          html = html.replace(/\n/g,'');
+          html = html.replace(/\s{3,}/g,'\n');
+          let fristScriptTapIndex = html.indexOf('</body');
+          let reg = new RegExp('(.{' + fristScriptTapIndex + '})')
+          html = html.replace(reg, `$1<script src="http://${this.ip?this.ip:'localhost'}:${this.port}/webpack-dev-server.js"></script>`);
+          data.html = html;
+          cb(null, data)
+        }
+      }
+    )
+  })
 }
 
 module.exports = webpackAutoRefreshPlugin;
